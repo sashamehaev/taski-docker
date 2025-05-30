f2dc130d825ed76524194754dcf35ad847cf9063
Имя Фамилия <alexandrmekhaev@yandex.ru>
2025-05-30 20:58:40 +0300

    prod

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['89.169.170.190', '127.0.0.1', 'localhost', 'sashamehaev-taski.bounceme.net']
+ALLOWED_HOSTS = ['89.169.170.190', '127.0.0.1', 'localhost', 'sashamehaev-taski.zapto.org']
 
 
 # Application definition
